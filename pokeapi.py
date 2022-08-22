0b5a16b3349d12a3e11fcc9ffe8a20c45f146dd8
rich <108325206+crich28@users.noreply.github.com>
2022-08-22 07:19:16 -0700

    cleaning

--- pokeapi.py
+++ pokeapi.py
@@ -1,7 +1,7 @@
-import json
-from flask import Flask, jsonify, request,render_template
+
+from flask import Flask
 import csv
-import random
+
 
 with open('pokeparser.csv', 'r') as csvfile:
             csvreader = csv.DictReader(csvfile)
@@ -9,35 +9,23 @@ with open('pokeparser.csv', 'r') as csvfile:
             for pokemon in csvreader:
                 pokedatajson.append(pokemon)
                
-namelist=[]
-for pokemon in pokedatajson:
-    
-        namelist.append(pokemon['name'])
 
-# creating a Flask app
 app = Flask(__name__)
   
 @app.route('/')
 def homepage():
-    pokename = random.choice(namelist)
-    for pokemon in pokedatajson:
-        if pokename in pokemon['name']:
-            return jsonify(pokemon)
+    return '<p>Pokemon Api by Chris S. type pokemon/"pokemonname" for specfic pokemon</p>'
 
-@app.route('/pokemon/<path:subpath>')
+@app.route('/pokemon/<subpath>')
 def pokemonpages(subpath):
 
     for pokemon in pokedatajson:
         if subpath in pokemon['name']:
             
-            return jsonify(pokemon)
-    # for pokemon in pokedatajson:
-    #     if name in pokemon['name']:
-            
+            return (pokemon)
 
-    #         return jsonify(pokemon)
 
 if __name__ == '__main__':
 
-	# running app
+
     app.run(use_reloader = True, debug = True)
